12c2246681499659e8c60f7e83c0eea6ffc33f9c
Ian Lockhart <ianlockhart@Ians-MacBook-Pro.local>
2021-12-08 20:26:19 -0700

    dependencies

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = express()
 app.use(express.static('./dist/Final_Angular_FE'))
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Final_Angular_FE'}))
+    res.sendFile('index.html', {root: 'dist/Final_Angular_FE/src'}))
 
 app.listen(process.env.PORT || 8080)
