5de77a44406cf19085adc6e7f5feca331d7e0b5a
Ian Lockhart <ianlockhart@Ians-MacBook-Pro.local>
2021-12-08 20:14:41 -0700

    server js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static('./dist/Final_Angular_FE'))
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/Final_Angular_FE'}))
+
+app.listen(process.env.PORT || 8080)
